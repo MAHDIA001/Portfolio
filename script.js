0e3c75b0ba4e09811bf2b4765863a8a1849cbab7
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-06 19:54:08 +0500

    mobile menu is coded

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 window.onload=function(){
-const nav = document.querySelector('.nav-items')
-const humburger = document.querySelector('.humburger')
+const nav = document.querySelector('.nav-items');
+const humburger = document.querySelector('.humburger');
+const closebtn = document.querySelector('.closebtn');
  humburger.addEventListener('click',()=>{
-     humburger.classList.toggle('active')
+     humburger.classList.toggle('active');
      nav.classList.toggle("active");
- })
+ });
+
+ document.querySelectorAll('.nav-item').forEach((n) =>
+   n.addEventListener('click', () => {
+     humburger.classList.remove('active');
+     nav.classList.remove('active');
+   })
+ );
 }
