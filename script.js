ffd2512cd8543d1be792aa8a9f7043501e8f60cb
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-06 20:45:09 +0500

    bugs fixed

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const nav = document.querySelector(".nav-items");
-const humburger = document.querySelector(".humburger");
-const closebtn = document.querySelector(".closebtn");
-humburger.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  nav.classList.toggle("active");
+const nav = document.querySelector('.nav-items');
+const humburger = document.querySelector('.humburger');
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  nav.classList.toggle('active');
 });
-document.querySelectorAll(".nav-item").forEach((n) =>
-  n.addEventListener("click", () => {
-    humburger.classList.remove("active");
-    nav.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-item').forEach((n) =>
+  n.addEventListener('click', () => {
+    humburger.classList.remove('active');
+    nav.classList.remove('active');
+  }));
+
