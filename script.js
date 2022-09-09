41c1c76daf9f0378f0268e654eea7b6bb1016306
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-09 22:10:27 +0500

    fix linter bugs

--- script.js
+++ script.js
@@ -9,17 +9,13 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   nav.classList.remove('active');
 }));
 
-
 // form validation
 const email = document.getElementById('mail');
 const lowerCase = email.value.toLowerCase();
 const submit = document.querySelector('.sub');
-
-submit.addEventListener('click', (e)=>{
-if(email!==lowerCase){
-  e.preventDefault();
-  document.querySelector('#text').innerHTML='error: email cannot be uppercase'
- 
-}
+submit.addEventListener('click', (e) => {
+if (email!== lowerCase) {
+    e.preventDefault();
+    document.querySelector('#text').innerHTML = 'error: email cannot be uppercase'
+};
 });
-
