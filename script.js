10e69045ea27f2449d4758f6ff15db451baa3639
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-09 22:02:51 +0500

    from valiation

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   humburger.classList.remove('active');
   nav.classList.remove('active');
 }));
+
+
+// form validation
+const email = document.getElementById('mail');
+const lowerCase = email.value.toLowerCase();
+const submit = document.querySelector('.sub');
+
+submit.addEventListener('click', (e)=>{
+if(email!==lowerCase){
+  e.preventDefault();
+  document.querySelector('#text').innerHTML='error: email cannot be uppercase'
+ 
+}
+});
+
