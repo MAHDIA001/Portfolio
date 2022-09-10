c1364ae3e7676766eaafc192847d4bd22ca1b071
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-10 07:19:27 +0500

    linters bug fixed

--- script.js
+++ script.js
@@ -21,23 +21,20 @@ submit.addEventListener('click', (e) => {
 });
 
 // local storage
-
 const name = document.getElementById('name');
-const textarea =  document.getElementById('textare');
-const newData = ()=> {
-  let nameobject = {
+const textarea = document.getElementById('textare');
+function newData() {
+  const nameobject = {
     firstName: `${name.value}`,
     emails: `${email.value}`,
     text: `${textarea.value}`,
   }; 
-  window.localStorage.setItem('nameobject', JSON.stringify(nameobject));
+ window.localStorage.setItem('nameobject', JSON.stringify(nameobject));
 }
-
 function getLocalStorage() {
-  let newObj =JSON.parse(window.localStorage.getItem("nameobject"));
+  const newObj =JSON.parse(window.localStorage.getItem('nameobject'));
   name.value = newObj.firstName;
   email.value = newObj.emails;
   textarea.value = newObj.text;
 }
 getLocalStorage();
-
