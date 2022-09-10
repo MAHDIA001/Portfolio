d8e4beca668ed8ba8ef6911de5f38a76a93127a3
Mahdia <59234266+MAHDIA001@users.noreply.github.com>
2022-09-10 08:57:40 +0500

    fix linters bug

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ const textarea = document.getElementById('textare');
 const form = document.getElementById('form');
 form.addEventListener('input', () => {
   const nameobject = {
-   firstName: `${name.value}`,
-   emails: `${email.value}`,
-   text: `${textarea.value}`,
+    firstName: `${name.value}`,
+    emails: `${email.value}`,
+    text: `${textarea.value}`,
   };
   localStorage.setItem('nameobject', JSON.stringify(nameobject));
 });
