6e238bb3d7d5f2314524f075c50449f612408537
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-10 08:16:49 +0500

    add scrpt to save data locally

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   humburger.classList.remove('active');
   nav.classList.remove('active');
 }));
+
+const name = document.getElementById('name')
+
+const textarea =  document.getElementById('textare')
+const name = document.getElementById('name');
+const form = document.getElementById('form')
+ form.addEventListener('input',()=> {
+  const nameobject = {
+    firstName: `${name.value}`,
+    emails: `${email.value}`,
+    text: `${textarea.value}`,
+  }; 
+  localStorage.setItem('nameobject', JSON.stringify(nameobject));
+});
+
+function getLocalStorage() {
+  const newObj =JSON.parse(window.localStorage.getItem('nameobject'));
+  name.value = newObj.firstName;
+  email.value = newObj.emails;
+  textarea.value = newObj.text;
+}
+getLocalStorage()
